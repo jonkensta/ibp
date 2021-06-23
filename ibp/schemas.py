e957a4927679f0139020e15ba3e016e51e98cc5d
Jonathan Starr <jonstarr67@gmail.com.com>
2021-06-23 13:41:54 -0500

    Add comment on each marshmallow schema field

--- ibp/schemas.py
+++ ibp/schemas.py
@@ -25,6 +25,7 @@ class UnitSchema(Schema):
     """:py:mod:`marshmallow` schema for :py:class:`ibp.models.Unit`."""
 
     id = fields.Int(dump_only=True)
+    """Read-only auto-incrementing unit index."""
 
     name = fields.Str()
     """Unit name encoded as a string."""
@@ -73,37 +74,78 @@ class InmateSchema(Schema):
     """:py:mod:`marshmallow` schema for :py:class:`ibp.models.Inmate`."""
 
     jurisdiction = fields.Str()
+    """Prison system holding the inmate."""
+
     id = fields.Int()
+    """Inmate's numeric identifier as used in their jurisdiction."""
 
     first_name = fields.Str()
+    """Inmate first name.
+
+    In some cases, this is given by the provider; in others cases, it is parsed
+    from the full name using :py:class:`nameparser.parser.HumanName`.
+
+    """
+
     last_name = fields.Str()
+    """Inmate last name.
+
+    In some cases, this is given as-is by the provider; in others cases, it is
+    parsed from the full name using :py:class:`nameparser.parser.HumanName`.
+
+    """
 
     sex = fields.Str()
-    url = fields.URL()
+    """Inmate gender as reported by provider."""
+
     race = fields.Str()
+    """Inmate race as reported by provider."""
+
+    url = fields.URL()
+    """Inmate URL where their information is web accessible."""
 
     release = fields.Str()
+    """Date of when this inmate is set to be released."""
+
     release_warning = fields.Function(warnings.inmate_pending_release)
+    """Warning if an inmate is to be released soon."""
 
     datetime_fetched = fields.Str()
+    """Datetime when inmate data was fetched from provider."""
+
     entry_age_warning = fields.Function(warnings.inmate_entry_age)
+    """Warning if an inmate's information is stale."""
 
     unit = fields.Nested(UnitSchema, only=["name", "url"])
+    """Prison unit holding the inmate."""
+
     lookups = fields.Nested(LookupSchema, many=True)
+    """List of lookups performed on this inmate by IBP volunteers."""
+
     comments = fields.Nested(CommentSchema, many=True)
+    """List of comments on this inmate made by IBP volunteers."""
+
     requests = fields.Nested(RequestSchema, many=True)
+    """List of requests made by this inmate."""
 
 
 class ShipmentSchema(Schema):
     """:py:mod:`marshmallow` schema for :py:class:`ibp.models.Shipment`."""
 
     date_shipped = fields.Date()
+    """Date that the shipment was made."""
 
     tracking_url = fields.Str()
+    """Shipping service tracking URL if available."""
+
     tracking_code = fields.Str()
+    """Shipping service tracking code if available."""
 
     weight = fields.Int()
+    """Weight of the shipment in ounces."""
+
     postage = fields.Int()
+    """Postage of the shipment in US cents."""
 
 
 request = RequestSchema(unknown="EXCLUDE")
